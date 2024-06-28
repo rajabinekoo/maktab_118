180bb38d0e90b8299b357e0d6790282e829bc9d8
rajabinekoo <ali.rajabinekoo@gmail.com>
2024-06-28 07:44:27 -0400

    for

--- 2-JS(condition,loop)/main.js
+++ 2-JS(condition,loop)/main.js
@@ -1,3 +1,5 @@
+// ============================ if else, switch case ============================
+
 // let type = prompt("Enter calculator type: (simple | advanced)");
 // if (!calcType) {
 //   calcType = "simple";
@@ -68,19 +70,85 @@
 
 // console.log(result);
 
-let exit = false;
-let round = 1;
-while (!exit) {
-  let command = prompt();
-  exit = command === "exit";
-  //   if (!exit) {
-  //     console.log(round);
-  //     round++;
-  //   }
-  //   if (exit) break;
-  if (exit || command === "a") continue;
-  console.log(round);
-  //   let oldValue = round++;
-  //   let currentValue = ++round;
-  round++;
-}
+// ============================ while ============================
+
+// let exit = false;
+// let round = 1;
+// while (!exit) {
+//   let command = prompt();
+//   exit = command === "exit";
+//   //   if (!exit) {
+//   //     console.log(round);
+//   //     round++;
+//   //   }
+//   //   if (exit) break;
+//   if (exit || command === "a") continue;
+//   console.log(round);
+//   //   let oldValue = round++;
+//   //   let currentValue = ++round;
+//   round++;
+// }
+
+// ============================ for ============================
+// for (start; stop; step)
+
+// iterator
+// increament
+// for (let i = 0; i < 5; i++) {
+//   console.log("The number is " + i);
+// }
+// iterator =        0 1 2 3 4 5
+// scope execution = 1 2 3 4 5 -
+
+// decreament
+// for (let i = 3; i >= 0; i--) {
+//   console.log("The number is " + i);
+// }
+// iterator =        3 2 1 0
+// scope execution = 1 2 3 4
+
+// ---------- IsPrime solution ----------
+// let number = Number(prompt("Enter your number:"));
+
+// let isPrime = true;
+// for (let i = 2; i < number; i++) {
+//   if (number % i === 0) {
+//     isPrime = false;
+//     break;
+//   }
+// }
+
+// bad idea - just continue example
+// for (let i = 1; i <= number; i++) {
+//   if (i === 1 || i === number) {
+//     continue;
+//   }
+//   if (number % i === 0) {
+//     isPrime = false;
+//     break;
+//   }
+// }
+
+// console.log(isPrime ? "Prime" : "Not prime");
+
+// if (isPrime) {
+//   console.log("prime");
+// } else {
+//   console.log("not prime");
+// }
+
+// ---------- for / while comparision ----------
+
+// let i = 1; //expression 1
+// //expression 2
+// for (; i <= 10; ) {
+//   console.log(i);
+//   i++; //expression 3
+// }
+
+// let i2 = 1; //expression 1
+// //expression 2
+// while (i2 <= 10) {
+//   console.log(i2);
+//   i2++; //expression 3
+// }
