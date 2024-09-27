657c9f38f6db9a61565844f6de51c9d448e2b311
rajabinekoo <ali.rajabinekoo@gmail.com>
2024-09-27 04:51:36 -0400

    Optimized form added

--- 23-react-fundamentals2/src/App.tsx
+++ 23-react-fundamentals2/src/App.tsx
@@ -1,7 +1,18 @@
-import { PostsList } from "./components/posts";
+import { Form } from "./components/form";
+
+// function App() {
+//   const [toggle, setToggle] = React.useState(true);
+
+//   return (
+//     <div className="bg-slate-100">
+//       <button onClick={() => setToggle(!toggle)}>toggle</button>
+//       {toggle && <OptimizedPostsList />}
+//     </div>
+//   );
+// }
 
 function App() {
-  return <PostsList />;
+  return <Form />;
 }
 
 export default App;
---
+++ 23-react-fundamentals2/src/components/form.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { Input } from "./input";
+
+interface IValuesType<T = string> {
+  creditCard: T;
+  quantity: T;
+}
+
+type validatorFunc = (_: string) => string | null;
+const validator: IValuesType<validatorFunc> = {
+  creditCard: (value: string) => {
+    if (isNaN(Number(value))) return "Invalid number";
+    return value?.length === 16 ? null : "Credit card length must be 16";
+  },
+  quantity: (value: string) => {
+    if (isNaN(Number(value))) return "Invalid number";
+    return Number(value) > 0 ? null : "Quantity must be positive number";
+  },
+};
+
+export const Form: React.FC = () => {
+  const [firstTimeLoading, setFirstTimeLoading] = React.useState<boolean>(true);
+  const [values, setValues] = React.useState<IValuesType>({
+    creditCard: "",
+    quantity: "",
+  });
+  const [errors, setErrors] = React.useState<IValuesType>({
+    creditCard: "",
+    quantity: "",
+  });
+
+  const onChangeHandler = (fieldName: keyof IValuesType, value: string) => {
+    const newValues = { ...values };
+    newValues[fieldName] = value;
+    setValues(newValues);
+  };
+
+  const onSubmitHandler: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    if (Object.values(errors).join("").length) return;
+    console.log(values);
+  };
+
+  React.useEffect(() => {
+    if (firstTimeLoading) {
+      setFirstTimeLoading(false);
+      return;
+    }
+    const newErrors = { ...errors };
+    const creditCardError = validator.creditCard(values.creditCard);
+    if (creditCardError) newErrors.creditCard = creditCardError;
+    else newErrors.creditCard = "";
+    const quantityError = validator.quantity(values.quantity);
+    if (quantityError) newErrors.quantity = quantityError;
+    else newErrors.quantity = "";
+    setErrors(newErrors);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [values]);
+
+  return (
+    <main className="bg-slate-100 min-h-screen">
+      <section className="space-y-5 container mx-auto max-w-[500px] py-10 px-4">
+        <form
+          onSubmit={onSubmitHandler}
+          className="bg-white rounded-md shadow-md px-6 py-4"
+        >
+          <p className="text-gray-800 font-semibold text-3xl mb-8">
+            Buyout Form
+          </p>
+          <div className="space-y-4">
+            <Input
+              type="text"
+              label="Credit Card"
+              placeholder="XXXX XXXX XXXX XXXX"
+              error={errors.creditCard}
+              onChange={(event) =>
+                onChangeHandler("creditCard", event.target.value)
+              }
+            />
+            <Input
+              type="text"
+              label="Quantity"
+              placeholder="ex: 2"
+              error={errors.quantity}
+              onChange={(event) =>
+                onChangeHandler("quantity", event.target.value)
+              }
+            />
+            <button
+              type="submit"
+              className="bg-slate-700 hover:!bg-slate-950 text-white font-semibold rounded-md px-2 py-1 w-full"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </main>
+  );
+};
