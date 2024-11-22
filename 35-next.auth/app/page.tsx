5d7c8b1afaba335a9ff3aa67a9136af4f567f72c
rajabinekoo <ali.rajabinekoo@gmail.com>
2024-11-22 04:39:34 -0500

    Fix

--- 35-next.auth/app/page.tsx
+++ 35-next.auth/app/page.tsx
@@ -1,10 +1,6 @@
 import { LoginBtn } from "@/components/login-btn";
-import { cookies } from "next/headers";
 
 export default async function Home() {
-  const cookie = await cookies();
-  console.log("[!] sessions", cookie.get("sessions"));
-
   return (
     <>
       <LoginBtn />
